516e1cbf634a8f76f59d254936c4eca9ac4d3761
arvidn <arvid@libtorrent.org>
2019-02-15 13:55:21 +0100

    add missing get_torrent_status() and refresh_torrent_status() to python binding

--- bindings/python/src/session.cpp
+++ bindings/python/src/session.cpp
@@ -469,17 +469,44 @@ namespace
 
     list get_torrents(lt::session& s)
     {
-        list ret;
         std::vector<torrent_handle> torrents;
         {
            allow_threading_guard guard;
            torrents = s.get_torrents();
         }
 
+        list ret;
         for (std::vector<torrent_handle>::iterator i = torrents.begin(); i != torrents.end(); ++i)
-        {
             ret.append(*i);
+        return ret;
+    }
+
+    list get_torrent_status(lt::session& s, object pred, int const flags)
+    {
+        std::vector<torrent_status> torrents;
+        s.get_torrent_status(&torrents, boost::bind(pred, _1), flags);
+
+        list ret;
+        for (std::vector<torrent_status>::iterator i = torrents.begin(); i != torrents.end(); ++i)
+            ret.append(*i);
+        return ret;
+    }
+
+    list refresh_torrent_status(lt::session& s, list in_torrents, int const flags)
+    {
+        std::vector<torrent_status> torrents;
+        int const n = boost::python::len(in_torrents);
+        for (int i = 0; i < n; ++i)
+           torrents.push_back(extract<torrent_status>(in_torrents[i]));
+
+        {
+           allow_threading_guard guard;
+           s.refresh_torrent_status(&torrents, flags);
         }
+
+        list ret;
+        for (std::vector<torrent_status>::iterator i = torrents.begin(); i != torrents.end(); ++i)
+            ret.append(*i);
         return ret;
     }
 
@@ -972,6 +999,8 @@ void bind_session()
         .def("get_ip_filter", allow_threads(&lt::session::get_ip_filter))
         .def("find_torrent", allow_threads(&lt::session::find_torrent))
         .def("get_torrents", &get_torrents)
+        .def("get_torrent_status", &get_torrent_status)
+        .def("refresh_torrent_status", &refresh_torrent_status)
         .def("pause", allow_threads(&lt::session::pause))
         .def("resume", allow_threads(&lt::session::resume))
         .def("is_paused", allow_threads(&lt::session::is_paused))
